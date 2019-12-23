649d7b7edff1bd330e609c803ac6180df25bb170
Pranjali Pokharel <pranjali@ualberta.ca>
2019-12-23 11:34:44 -0700

    Remove test code and add liability totals in controller.

--- CraftyFinanceApi/Controllers/LiabilityController.cs
+++ CraftyFinanceApi/Controllers/LiabilityController.cs
@@ -7,28 +7,25 @@ using Microsoft.Extensions.Logging;
 
 namespace CraftyFinanceApi.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class LiabilityController: ControllerBase
+  [ApiController]
+  [Route("[controller]")]
+  public class LiabilityController : ControllerBase
+  {
+    private readonly ILogger<LiabilityController> _logger;
+
+    public LiabilityController(ILogger<LiabilityController> logger)
     {
-        private static Liability Liabilities = new Liability
-        {
-            Mortgage1 = 3,
-            Mortgage1Interest = 55,
-            Mortgage2 = 300.5m,
-            Mortgage2Interest = 20,
-        };
-        private readonly ILogger<LiabilityController> _logger;
-
-        public LiabilityController(ILogger<LiabilityController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet]
-        public ActionResult<Liability> Get()
-        {
-            return Ok(Liabilities);
-        }
+      _logger = logger;
     }
+
+    [HttpPost]
+    public ActionResult<decimal> GetTotalLiabilities(Liability liability)
+    {
+      var liabilityTotals = liability.CreditCard1 + liability.CreditCard2 +
+        liability.OtherCredit + liability.Mortgage1 + liability.Mortgage2 +
+        liability.LineOfCredit + liability.InvestmentLoan +
+        liability.StudentLoan + liability.CarLoan;
+      return Ok(liabilityTotals);
+    }
+  }
 }
